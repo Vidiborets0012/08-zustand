a54a6d11bdf43362fdae57d7319f00eed99b0b56
Vidiborets Viktor <vidiboretsv@gmail.com>
2025-12-16 16:24:43 +0200

    move the logic of creating a new note from the modal window to a separate route app/notes/action/create/page.tsx

---
+++ app/notes/action/create/page.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import NoteForm from "@/components/NoteForm/NoteForm";
+import css from "./CreateNote.module.css";
+
+export default function CreateNotePage() {
+  return (
+    <main className={css.main}>
+      <div className={css.container}>
+        <h1 className={css.title}>Create note</h1>
+
+        <NoteForm onClose={() => history.back} />
+      </div>
+    </main>
+  );
+}
