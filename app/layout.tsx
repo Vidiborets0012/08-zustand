aecc8ca09ba50a29f6b2c0bba0c7d6c4a788be33
Vidiborets Viktor <vidiboretsv@gmail.com>
2025-12-16 08:57:04 +0200

    add meta tags and Open Graph for app/layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,6 +18,20 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "NoteHub",
   description: "Manage your notes efficiently",
+  openGraph: {
+    title: "NoteHub",
+    description: "Simple and fast note management",
+    url: "https://08-zustand-olive-one.vercel.app/",
+    images: [
+      {
+        url: "https://ac.goit.global/fullstack/react/notehub-og-meta.jpg",
+        width: 1200,
+        height: 630,
+        alt: "NoteHub — personal notes app",
+      },
+    ],
+    type: "website",
+  },
 };
 
 export default function RootLayout({
